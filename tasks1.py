de8355d38de7b427724a81d20772ec8b3d4a2303
Tumani1337 <idi_suda06@mail.ru>
2025-04-11 17:43:20 +0300

    add is_polindrom

--- tasks1.py
+++ tasks1.py
@@ -39,3 +39,20 @@ def count_ones(number: int) -> int:
             result += 1
 
     return result
+
+def is_palindrom(n: int) -> True or False:
+
+    lst_n = []
+    result = False
+
+    while n != 0:
+        lst_n.append(n % 10)
+
+        n = n // 10
+
+    tls_n = lst_n[:: -1]
+
+    if tls_n == lst_n:
+        result = True
+
+    return  result
