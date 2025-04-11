5b5e3ca27ff811a0ff8e8107a1018d6a2418157f
Tumani1337 <idi_suda06@mail.ru>
2025-04-11 17:37:50 +0300

    add factorial

--- tasks1.py
+++ tasks1.py
@@ -8,3 +8,18 @@ def factorial(number: int) -> int:
         result *= element
 
     return result
+
+def fibonacci(number: int) -> list:
+
+    if number < 0: raise ValueError
+
+    result = [0, 1]
+    if number == 0: result = [0]
+
+    for element in range(number):
+        fib_number = result[-1] + result[-2]
+        result.append(fib_number)
+
+    if number != 0: result.remove(result[-1])
+
+    return result
